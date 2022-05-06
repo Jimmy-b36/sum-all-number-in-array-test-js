2d4cec94861222864bc37fac6077371d20752657
James Ball <jamie.j.ball@gmail.com>
2022-05-06 08:46:41 -0700

    function to sum arrays using recursion

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let i = array.length;
+  console.log(i, array[i - 1]);
+
+  if (!i) {
+    console.log("finito");
+    return 0;
+  }
+
+  let value = array[i - 1];
+  if (Array.isArray(value)) {
+    value = sumItems(value);
+    console.log("array value =", value);
+  }
+
+  const newArray = array.slice(0, -1);
+  return value + sumItems(newArray);
 }
 
+// const array = [1, 2, [3, 4], 5];
+// // const array = [1, 2, 3];
+// const result = sumItems(array);
+// console.log(result);
 module.exports = sumItems;
